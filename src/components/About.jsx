e7d9b59847c7117546f3f206196cf2f936d2438d
kunal Saini <kunalsaini@MacBook-Air.local>
2024-08-14 21:24:57 +0530

    changed some animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,47 @@ import {motion} from "framer-motion";
 const About = () => {
     return (
         <div className="border-b border-neutral-900 pb-4">
-            <h2 className="my-20 text-center text-4xl">About
-                <span className="text-neutral-500"> Me</span></h2>
+            <h2 className="flex flex-wrap justify-center items-center my-20 text-center text-4xl">
+                <motion.div
+                whileInView={{opacity:1,x:0,y:0}}
+                initial={{x:-100,y:-30,opacity:0}}
+                transition={{duration:0.5}} 
+                >A</motion.div>
+                <motion.div
+                whileInView={{opacity:1,x:0,y:0}}
+                initial={{x:-60,y:-30,opacity:0}}
+                transition={{duration:0.5}}
+                >b</motion.div>
+                <motion.div
+                whileInView={{opacity:1,x:0,y:0}}
+                initial={{x:-30,y:-30,opacity:0}}
+                transition={{duration:0.5}}
+                >o</motion.div>
+                <motion.div
+                whileInView={{opacity:1,x:0,y:0}}
+                initial={{x:0,y:-30,opacity:0}}
+                transition={{duration:0.5}}
+                >u</motion.div>
+                <motion.div
+                whileInView={{opacity:1,x:0,y:0}}
+                initial={{x:30,y:-30,opacity:0}}
+                transition={{duration:0.5}}
+                >t</motion.div>
+                <div className="w-2"></div>
+                <motion.div
+                whileInView={{opacity:1,x:0,y:0}}
+                initial={{x:60,y:-30,opacity:0}}
+                transition={{duration:0.5}}
+                className="text-neutral-500"
+                >M</motion.div>
+                <motion.div
+                whileInView={{opacity:1,x:0,y:0}}
+                initial={{x:100,y:-30,opacity:0}}
+                transition={{duration:0.5}}
+                className="text-neutral-500"
+                >e</motion.div>
+                {/* <span className="text-neutral-500"> Me</span> */}
+                </h2>
             <div className="flex flex-wrap">
                 <motion.div
                 whileInView={{opacity:1,x:0}}
